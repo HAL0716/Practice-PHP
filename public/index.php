9839b8d527c569cd25366001cd8a33eda0037f32
HAL0716 <207191136+HAL0716@users.noreply.github.com>
2026-02-21 13:56:41 +0900

    feat: add basic routing and display phpinfo on root path

--- public/index.php
+++ public/index.php
@@ -1,1 +1,17 @@
-<?= phpinfo();
+<?php
+
+declare(strict_types=1);
+
+$url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$url = rtrim($url, '/');
+
+switch ($url) {
+    case '':
+        phpinfo();
+        exit;
+
+    default:
+        http_response_code(404);
+        echo '404 Not Found';
+        exit;
+}
