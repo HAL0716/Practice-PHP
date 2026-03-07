88a47c9634b1ca64e1b9b0b10813f364a63be3a7
HAL0716 <207191136+HAL0716@users.noreply.github.com>
2026-03-07 15:52:09 +0900

    refactor: ensure early return in HomeController index method for better flow

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -24,6 +24,7 @@ class HomeController extends Controller
                     'posts' => PostRepository::findAll(),
                 ]
             );
+            return;
         }
 
         $form = new PostForm();
