43434d2bdf8bdee5c73ba7a8de6ada1614505bf6
HAL0716 <207191136+HAL0716@users.noreply.github.com>
2026-03-22 13:18:49 +0900

    feat: update autoload configuration and remove unused Autoloader class

--- public/index.php
+++ public/index.php
@@ -2,16 +2,13 @@
 
 declare(strict_types=1);
 
-use App\Core\Autoloader;
+require_once __DIR__ . '/../vendor/autoload.php';
+
 use App\Core\Http\Request;
 use App\Core\Http\Session;
 use App\Constants\Routes;
 use App\Controllers\UserController;
 use App\Controllers\PostController;
-
-require_once __DIR__ . '/../src/Core/Autoloader.php';
-
-Autoloader::register();
 
 Session::init();
 
